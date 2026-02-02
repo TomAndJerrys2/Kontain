ef5ee89f684749437e2e1133cf9c5ef4d109c62d
TomAndJerrys2 <kylev2005@hotmail.com>
2026-02-02 12:09:56 +0000

    :CLI usage

--- kontain.c
+++ kontain.c
@@ -11,8 +11,29 @@ int main(int argc, char** argv) {
 	int8_t last_optiond = 0;
 
 	while((option = getopt(argc, argv, "c:m:u"))) {
+		switch(option) {
+			case 'c':
+				config.argc = argc - last_optiond - 1;
+				config.argv = &argv[argc - config.argc];
+				goto finish_options;
+
+			case 'm':
+				config.mount_dir = optarg;
+				break;
+
+			case 'u':
+				if(sscanf(optarg, "%d", &config.uid) != 1) {
+					fprintf(stderr, "Kontain: UID in Wrong Format: Arg > %s\n", optarg);
+					goto usage;
+				}
+
+				break;
+
+			default:
+				goto usage;
+		}	
 		
-		
+		last_optiond = optind;	
 	}	
 
 	return 0;
