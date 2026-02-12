0d06fe0198c7c5522d2c775e0652456cd7eb33b1
TomAndJerrys2 <kylev2005@hotmail.com>
2026-02-12 12:50:32 +0000

    Check Namespace / Linux func

--- kontain.c
+++ kontain.c
@@ -35,45 +35,41 @@ int main(int argc, char **argv) {
     last_optiond = optind;
   }
 
-  // the gotos here are technically unsafe jumps - will improve upon this late
-  // with function ptrs
-	finish_options: 
-		if(!config.argc) goto usage;
-		if(!config.mount_dir) goto usage;	
-	
-		// todo
-		check_linux();
+// the gotos here are technically unsafe jumps - will improve upon this late
+// with function ptrs
+finish_options:
+  if (!config.argc)
+    goto usage;
+  if (!config.mount_dir)
+    goto usage;
 
-		char hostname[256] = {0};
-		if(choose_hostname(hostname, sizeof(hostname)) goto error;
+  // todo
+  check_linux();
 
-		config.hostname = hostname;
-		
-		if(socketpair(AF_LOCAL, SOCK_SEQPACKET, 0, sockets)) { 
-			fprintf(stderr, "SocketPairs failed: %m\n");
-			goto error;
-		} 
+  char hostname[256] = {0};
+  if(choose_hostname(hostname, sizeof(hostname)) 
+	goto error;
 
-		if(fcntl(sockets[0], F_SETFID, FD_CLOEXEC)) {
-			fprintf(stderr, "FNCTL failed: %m\n");
-			goto error;
-		}
+  config.hostname = hostname;
+ 
+  check_namespace(sockets, SOCKET_SIZE);
 
-		config.fd = sockets[1];
-
-		goto cleanup;
+  goto cleanup;
 		
   usage:
-		fprintf(stderr, "Kontain >> Usage: %s -u/-l/-m/./-c /bin/sh ~\n", argv[0]);
+    fprintf(stderr, "Kontain >> Usage: %s -u/-l/-m/./-c /bin/sh ~\n", argv[0]);
 
-	error:
-		err = 1;
+  error:
+    err = 1;
 
   cleanup:
-		if(sockets[0]) close(sockets[0]);
-		if(sockets[1]) close(sockets[1]);
+     if(sockets[0]) 
+        close(sockets[0]);
+		
+     if(sockets[1]) 
+        close(sockets[1]);
 
-		return err;
+     return err;
 
   return 0;
 }
@@ -82,55 +78,70 @@ int main(int argc, char **argv) {
 // version - will in future provide autmatic updates for lower/
 // higher version of the linux kernel for portability
 void check_linux(void) {
-	fprintf(stderr, "=> Validating Linux Kernel Version...");
+  fprintf(stderr, "=> Validating Linux Kernel Version...");
 
-	struct machine_name host  = {0};
+  struct machine_name host = {0};
 
-	if(uname(&host)) {
-		fprintf(stderr, "Failed > %m\n");
-		goto cleanup;
-	}	
+  if (uname(&host)) {
+    fprintf(stderr, "Failed > %m\n");
+    goto cleanup;
+  }
 
-	int major = -1, minor = -1;	
+  int major = -1, minor = -1;
 
-	if(sscanf(host.release, "%u.%u.", &major, &minor) != 2) {
-		fprintf(stderr, "Release format not accepted > %s\n", host.release);
-		goto cleanup;
-	}
+  if (sscanf(host.release, "%u.%u.", &major, &minor) != 2) {
+    fprintf(stderr, "Release format not accepted > %s\n", host.release);
+    goto cleanup;
+  }
 
-	if(major != 4 || (minor != 7 && minor != 8)) {
-		fprintf(stderr, "Expected 4.7.X or 4.8.X > %s\n", host.release);
-		goto cleanup;
-	}
+  if (major != 4 || (minor != 7 && minor != 8)) {
+    fprintf(stderr, "Expected 4.7.X or 4.8.X > %s\n", host.release);
+    goto cleanup;
+  }
 
-	if(strcmp("x86_64", host.machine)) {
-		fprintf(stderr, "Expected Architecure: x86_64 got > %s\n", host.machine);
-		goto cleanup;
-	}
+  if (strcmp("x86_64", host.machine)) {
+    fprintf(stderr, "Expected Architecure: x86_64 got > %s\n", host.machine);
+    goto cleanup;
+  }
 
-	fprintf(stderr, "%s > %s\n", host.release, host.machine);
+  fprintf(stderr, "%s > %s\n", host.release, host.machine);
 }
 
 // declares hostname based on the span passed and chooses
 // from static instances of prenamed machine hosts
-int choose_hostname(char* buffer, const size_t length) {
-	static const char* suits[] = {};
-	static const char* minor[] = {};
-	static const char* major[] = {};
+int choose_hostname(char *buffer, const size_t length) {
+  static const char *suits[] = {};
+  static const char *minor[] = {};
+  static const char *major[] = {};
 
-	struct time_spec now = {0};
+  struct time_spec now = {0};
 
-	clock_get_time(CLOCK_MONOTONIC, &now);
-	size_t now_spec = now.tv_nsec % 78;
+  clock_get_time(CLOCK_MONOTONIC, &now);
+  size_t now_spec = now.tv_nsec % 78;
 
-	if(ix < sizeof(major) / sizeof(*major))
-		snprintf(buffer, length, "%051x-%s", now.tv_sec, major[ix]);
-	else {
-		ix -= sizeof(major) / sizeof(*major);
-		snprintf(buffer, length, "%051x-%s-of-%s", 
-				now.tv_sec, minor[ix % (sizeof(minor) / sizeof(*minor))], 
-					    suits[ix % (sizeof(minor) / sizeof(*minor))]);
-	}
+  if (ix < sizeof(major) / sizeof(*major))
+    snprintf(buffer, length, "%051x-%s", now.tv_sec, major[ix]);
+  else {
+    ix -= sizeof(major) / sizeof(*major);
+    snprintf(buffer, length, "%051x-%s-of-%s", now.tv_sec,
+             minor[ix % (sizeof(minor) / sizeof(*minor))],
+             suits[ix % (sizeof(minor) / sizeof(*minor))]);
+  }
 
-	return 0;
+  return 0;
+}
+
+void check_namespace(const int *socket, const size_t ssize) {
+
+  if (socketpair(AF_LOCAL, SOCK_SEQPACKET, 0, sockets)) {
+    fprintf(stderr, "SocketPairs failed: %m\n");
+    goto error;
+  }
+
+  if (fcntl(sockets[0], F_SETFID, FD_CLOEXEC)) {
+    fprintf(stderr, "FNCTL failed: %m\n");
+    goto error;
+  }
+
+  config.fd = sockets[1];
 }
